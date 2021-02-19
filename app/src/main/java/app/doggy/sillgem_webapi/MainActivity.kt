20e789bac51ce14237c0ddf3f53ba45d68b37d13
Kaito-Dogi <kendobu0405@gmail.com>
2021-02-20 01:35:57 +0900

    complete

--- app/src/main/java/app/doggy/sillgem_webapi/MainActivity.kt
+++ app/src/main/java/app/doggy/sillgem_webapi/MainActivity.kt
@@ -2,9 +2,14 @@ package app.doggy.sillgem_webapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import coil.api.load
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -22,6 +27,40 @@ class MainActivity : AppCompatActivity() {
                 .build()
         val userService: UserService = retrofit.create(UserService::class.java)
 
+        requestButton.setOnClickListener {
 
+            /*
+            * メインスレッド
+            * 画面を更新する担当
+            * 順番に処理される
+            * 重たい処理をすると画面の更新が止まる
+            * 5秒以上止めてはいけない
+            */
+
+            /*
+            * 別スレッド
+            * 重たい処理を実行する
+            * メインスレッドを止めない
+            * 画面の更新を行うことはできない
+            */
+
+            //runBlocking内
+            runBlocking(Dispatchers.IO) {
+                runCatching {
+                    userService.getUser(userIdEditText.text.toString())
+                }
+            }.onSuccess {
+                //成功した時（runBlocking外）
+                avatarImageView.load(it.avatarUrl)
+                nameTextView.text = it.name
+                userIdTextView.text = it.userId
+                followingTextView.text = it.following.toString()
+                followersTextView.text = it.followers.toString()
+            }.onFailure {
+                //失敗した時（runBlocking外）
+                Toast.makeText(applicationContext, "失敗", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
--- app/src/main/java/app/doggy/sillgem_webapi/UserService.kt
+++ app/src/main/java/app/doggy/sillgem_webapi/UserService.kt
@@ -5,8 +5,10 @@ import retrofit2.http.Path
 
 interface UserService {
 
-    //ユーザー情報を取得するメソッド。
-    //GitHubのユーザー名を引数にとって、取得したユーザー情報であるUserクラスを返す。
+    /*
+    * ユーザー情報を取得するメソッド。
+    * GitHubのユーザー名を引数にとって、取得したユーザー情報であるUserクラスを返す。
+    */
 
     //GET：HTTPリクエストの一つ。サーバーから情報を取得する。
     @GET("users/{userId}")
